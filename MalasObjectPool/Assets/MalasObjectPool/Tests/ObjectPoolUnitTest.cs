79fc8afa0c791ab76f4e034261960f1c55434db0
Michael Fuglsang <Michael Fuglsang>
2016-10-24 01:29:29 +0200

    BDD stylized tests.. sorta

--- MalasObjectPool/Assets/MalasObjectPool/Tests/ObjectPoolUnitTest.cs
+++ MalasObjectPool/Assets/MalasObjectPool/Tests/ObjectPoolUnitTest.cs
@@ -8,288 +8,336 @@ using Debug = UnityEngine.Debug;
 #if UNITY_EDITOR
 public partial class ObjectPool : MonoBehaviour
 {
-	public bool RunTests = true;
+    public bool RunTests = true;
 
-	void Start()
-	{
-		if (RunTests)
-		{
-			Debug.Log("*** Running Object Pool Unit Tests...");
+    void Start()
+    {
+        if (RunTests)
+        {
+            Debug.Log("*** Running Object Pool Unit Tests...");
 
-			StartCoroutine(DefaultConstructorObject());
-			StartCoroutine(DefaultConstructorObjectMany());
-			StartCoroutine(ThresholdDefaultSuccess1());
-			StartCoroutine(ThresholdDefaultSuccess2());
-			StartCoroutine(ThresholdIncreased1());
-			StartCoroutine(ThresholdIncreased2());
-			StartCoroutine(Release1());
-			StartCoroutine(Release2());
-			StartCoroutine(GameObjectTest1());
-			StartCoroutine(GameObjectTest2());
-			StartCoroutine(AsyncImmediateInstantiation());
-			StartCoroutine(AddTypeToPoolTest());
+            StartCoroutine(DefaultConstructorObject());
+            StartCoroutine(DefaultConstructorObjectMany());
+            StartCoroutine(ThresholdDefaultSuccess1());
+            StartCoroutine(ThresholdDefaultSuccess2());
+            StartCoroutine(ThresholdIncreased1());
+            StartCoroutine(ThresholdIncreased2());
+            StartCoroutine(Release1());
+            StartCoroutine(Release2());
+            StartCoroutine(GameObjectTest1());
+            StartCoroutine(GameObjectTest2());
+            StartCoroutine(AsyncImmediateInstantiation());
+            StartCoroutine(AddTypeToPoolTest());
 
-			PerformanceTest();
-		}
-	}
+            PerformanceTest();
+        }
+    }
 
-	IEnumerator DefaultConstructorObject()
-	{
-		TestDataClass data = ObjectPool.Instance.Acquire<TestDataClass>();
-		yield return new WaitForEndOfFrame();
-		TestDataClass expected = new TestDataClass(1);
+    IEnumerator DefaultConstructorObject()
+    {
+        //Given
+        TestDataClass data = ObjectPool.Instance.Acquire<TestDataClass>();
+        yield return new WaitForEndOfFrame();
+        TestDataClass expected = new TestDataClass(1);
 
-		Assert.IsTrue(expected.CheckEqual(data));
-	}
+        //When
+        bool equal = expected.CheckEqual(data);
 
-	IEnumerator DefaultConstructorObjectMany()
-	{
-		List<TestDataClass> list = new List<TestDataClass>();
-		TestDataClass expected = new TestDataClass(1);
-		int count = 20;
-		for (int i = 0; i < count; i++)
-		{
-			list.Add(ObjectPool.Instance.Acquire<TestDataClass>());
-			yield return new WaitForEndOfFrame();
-		}
+        //Then
+        Assert.IsTrue(equal);
+    }
 
-		bool defaultConstructorObjectMany = true;
+    IEnumerator DefaultConstructorObjectMany()
+    {
+        //Given
+        List<TestDataClass> list = new List<TestDataClass>();
+        TestDataClass expected = new TestDataClass(1);
+        int count = 20;
+        for (int i = 0; i < count; i++)
+        {
+            list.Add(ObjectPool.Instance.Acquire<TestDataClass>());
+            yield return new WaitForEndOfFrame();
+        }
 
-		foreach (TestDataClass d in list)
-		{
-			if (!expected.CheckEqual(d))
-				defaultConstructorObjectMany = false;
-		}
+        //When
+        bool defaultConstructorObjectMany = true;
+        foreach (TestDataClass d in list)
+        {
+            if (!expected.CheckEqual(d))
+                defaultConstructorObjectMany = false;
+        }
 
-		Assert.IsTrue(defaultConstructorObjectMany);
-	}
+        //Then
+        Assert.IsTrue(defaultConstructorObjectMany);
+    }
 
-	IEnumerator ThresholdDefaultSuccess1()
-	{
-		int expected = 32;
-		for (int i = 0; i < 31; i++)
-		{
-			ObjectPool.Instance.Acquire<ThresholdDerivativeDefaultSuccess1>();
-			yield return new WaitForEndOfFrame();
-		}
+    IEnumerator ThresholdDefaultSuccess1()
+    {
+        //Given
+        for (int i = 0; i < 31; i++)
+        {
+            ObjectPool.Instance.Acquire<ThresholdDerivativeDefaultSuccess1>();
+            yield return new WaitForEndOfFrame();
+        }
 
-		Assert.AreEqual(ObjectPool.Instance.GetInstanceCountTotal<ThresholdDerivativeDefaultSuccess1>(), expected);
-	}
+        //When
+        int count = ObjectPool.Instance.GetInstanceCountTotal<ThresholdDerivativeDefaultSuccess1>();
 
-	IEnumerator ThresholdDefaultSuccess2()
-	{
-		int expected = 64;
-		for (int i = 0; i < 32; i++)
-		{
-			ObjectPool.Instance.Acquire<ThresholdDerivativeDefaultSuccess2>();
-			yield return new WaitForEndOfFrame();
-		}
+        //Then
+        int expected = 32; //Count should be N * 2 of acquired items
+        Assert.AreEqual(count, expected);
+    }
 
-		Assert.AreEqual(ObjectPool.Instance.GetInstanceCountTotal<ThresholdDerivativeDefaultSuccess2>(), expected);
-	}
+    IEnumerator ThresholdDefaultSuccess2()
+    {
+        //Given
+        for (int i = 0; i < 32; i++)
+        {
+            ObjectPool.Instance.Acquire<ThresholdDerivativeDefaultSuccess2>();
+            yield return new WaitForEndOfFrame();
+        }
 
-	IEnumerator ThresholdIncreased1()
-	{
-		int expected = 16;
-		ObjectPool.Instance.Acquire<ThresholdDerivativeIncreased1>();
-		yield return new WaitForEndOfFrame();
-		ObjectPool.Instance.SetLowerInstantiationThreshold<ThresholdDerivativeIncreased1>(2);
+        //When
+        int count = ObjectPool.Instance.GetInstanceCountTotal<ThresholdDerivativeDefaultSuccess2>();
 
-		for (int i = 0; i < 14; i++)
-		{
-			ObjectPool.Instance.Acquire<ThresholdDerivativeIncreased1>();
-			yield return new WaitForEndOfFrame();
-		}
+        //Then
+        int expected = 64; //Count should be N * 2 of acquired items
+        Assert.AreEqual(count, expected);
+    }
 
-		Assert.AreEqual(ObjectPool.Instance.GetInstanceCountTotal<ThresholdDerivativeIncreased1>(), expected);
-	}
+    IEnumerator ThresholdIncreased1()
+    {
+        //Given
+        ObjectPool.Instance.Acquire<ThresholdDerivativeIncreased1>();
+        yield return new WaitForEndOfFrame();
+        ObjectPool.Instance.SetLowerInstantiationThreshold<ThresholdDerivativeIncreased1>(2);
 
-	IEnumerator ThresholdIncreased2()
-	{
-		int expected = 32;
-		ObjectPool.Instance.Acquire<ThresholdDerivativeIncreased2>();
-		yield return new WaitForEndOfFrame();
-		ObjectPool.Instance.SetLowerInstantiationThreshold<ThresholdDerivativeIncreased2>(2);
+        for (int i = 0; i < 14; i++)
+        {
+            ObjectPool.Instance.Acquire<ThresholdDerivativeIncreased1>();
+            yield return new WaitForEndOfFrame();
+        }
 
-		for (int i = 0; i < 15; i++)
-		{
-			ObjectPool.Instance.Acquire<ThresholdDerivativeIncreased2>();
-			yield return new WaitForEndOfFrame();
-		}
+        //When
+        int count = ObjectPool.Instance.GetInstanceCountTotal<ThresholdDerivativeIncreased1>();
 
-		Assert.AreEqual(ObjectPool.Instance.GetInstanceCountTotal<ThresholdDerivativeIncreased2>(), expected);
-	}
+        //Then
+        int expected = 16;
+        Assert.AreEqual(count, expected);
+    }
 
-	IEnumerator Release1()
-	{
-		int expected = 16;
-		ReleaseDerivative1 obj = ObjectPool.Instance.Acquire<ReleaseDerivative1>();
-		ObjectPool.Instance.SetLowerInstantiationThreshold<ReleaseDerivative1>(2);
+    IEnumerator ThresholdIncreased2()
+    {
+        //Given
+        ObjectPool.Instance.Acquire<ThresholdDerivativeIncreased2>();
+        yield return new WaitForEndOfFrame();
+        ObjectPool.Instance.SetLowerInstantiationThreshold<ThresholdDerivativeIncreased2>(2);
 
-		for (int i = 0; i < 13; i++)
-		{
-			ObjectPool.Instance.Acquire<ReleaseDerivative1>();
-			yield return new WaitForEndOfFrame();
-		}
+        for (int i = 0; i < 15; i++)
+        {
+            ObjectPool.Instance.Acquire<ThresholdDerivativeIncreased2>();
+            yield return new WaitForEndOfFrame();
+        }
 
-		ObjectPool.Instance.Release<ReleaseDerivative1>(obj);
-		ObjectPool.Instance.Acquire<ReleaseDerivative1>();
-		yield return new WaitForEndOfFrame();
+        //When
+        int count = ObjectPool.Instance.GetInstanceCountTotal<ThresholdDerivativeIncreased2>();
 
-		Assert.AreEqual(ObjectPool.Instance.GetInstanceCountTotal<ReleaseDerivative1>(), expected);
-	}
+        //Then
+        int expected = 32;
+        Assert.AreEqual(count, expected);
+    }
 
-	IEnumerator Release2()
-	{
-		int expected = 16;
-		ReleaseDerivative2 obj = ObjectPool.Instance.Acquire<ReleaseDerivative2>();
-		yield return new WaitForEndOfFrame();
-		ObjectPool.Instance.SetLowerInstantiationThreshold<ReleaseDerivative2>(2);
+    IEnumerator Release1()
+    {
+        //Given
+        ReleaseDerivative1 obj = ObjectPool.Instance.Acquire<ReleaseDerivative1>();
+        ObjectPool.Instance.SetLowerInstantiationThreshold<ReleaseDerivative1>(2);
 
-		for (int i = 0; i < 14; i++)
-		{
-			ObjectPool.Instance.Acquire<ReleaseDerivative2>();
-			yield return new WaitForEndOfFrame();
-		}
+        for (int i = 0; i < 13; i++)
+        {
+            ObjectPool.Instance.Acquire<ReleaseDerivative1>();
+            yield return new WaitForEndOfFrame();
+        }
 
-		ObjectPool.Instance.Release<ReleaseDerivative2>(obj);
-		ObjectPool.Instance.Acquire<ReleaseDerivative2>();
-		yield return new WaitForEndOfFrame();
+        //When
+        ObjectPool.Instance.Release<ReleaseDerivative1>(obj);
+        ObjectPool.Instance.Acquire<ReleaseDerivative1>();
+        yield return new WaitForEndOfFrame();
 
-		Assert.AreNotEqual(ObjectPool.Instance.GetInstanceCountTotal<ReleaseDerivative2>(), expected);
-	}
+        int count = ObjectPool.Instance.GetInstanceCountTotal<ReleaseDerivative1>();
 
-	IEnumerator GameObjectTest1()
-	{
-		ObjectPoolGameObjectTestScript obj = ObjectPool.Instance.Acquire<ObjectPoolGameObjectTestScript>();
-		yield return new WaitForEndOfFrame();
+        //Then
+        int expected = 16;
+        Assert.AreEqual(count, expected);
+    }
 
-		Assert.IsNotNull(obj);
-		Assert.IsNotNull(obj.gameObject);
-	}
+    IEnumerator Release2()
+    {
+        //Given
+        ReleaseDerivative2 obj = ObjectPool.Instance.Acquire<ReleaseDerivative2>();
+        yield return new WaitForEndOfFrame();
+        ObjectPool.Instance.SetLowerInstantiationThreshold<ReleaseDerivative2>(2);
 
-	IEnumerator GameObjectTest2()
-	{
-		for (int i = 0; i < 20; i++)
-		{
-			ObjectPoolGameObjectTestScript obj = ObjectPool.Instance.Acquire<ObjectPoolGameObjectTestScript>();
-			yield return new WaitForEndOfFrame();
+        for (int i = 0; i < 14; i++)
+        {
+            ObjectPool.Instance.Acquire<ReleaseDerivative2>();
+            yield return new WaitForEndOfFrame();
+        }
 
-			Assert.IsNotNull(obj);
-			Assert.IsNotNull(obj.gameObject);
-		}
-	}
+        //When
+        ObjectPool.Instance.Release<ReleaseDerivative2>(obj);
+        ObjectPool.Instance.Acquire<ReleaseDerivative2>();
+        yield return new WaitForEndOfFrame();
 
-	IEnumerator AsyncImmediateInstantiation()
-	{
-		yield return new WaitForEndOfFrame();
+        int count = ObjectPool.Instance.GetInstanceCountTotal<ReleaseDerivative2>();
 
-		TestDataClass expected = new TestDataClass(1);
+        //Then
+        int expected = 16;
+        Assert.AreNotEqual(count, expected);
+    }
 
-		for (int i = 0; i < 20; i++)
-		{
-			AsyncImmediateDerivative obj = ObjectPool.Instance.Acquire<AsyncImmediateDerivative>();
+    IEnumerator GameObjectTest1()
+    {
+        //Given
+        ObjectPoolGameObjectTestScript obj = ObjectPool.Instance.Acquire<ObjectPoolGameObjectTestScript>();
+        yield return new WaitForEndOfFrame();
 
-			Assert.IsTrue(obj.CheckEqual(expected));
-		}
-	}
+        //Then
+        Assert.IsNotNull(obj);
+        Assert.IsNotNull(obj.gameObject);
+    }
 
-	IEnumerator AddTypeToPoolTest()
-	{
-		yield return new WaitForEndOfFrame();
+    IEnumerator GameObjectTest2()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            //Given
+            ObjectPoolGameObjectTestScript obj = ObjectPool.Instance.Acquire<ObjectPoolGameObjectTestScript>();
+            yield return new WaitForEndOfFrame();
 
-		AddTypeToPoolDerivative d = new AddTypeToPoolDerivative();
-		ObjectPool.Instance.AddTypeToPool(d);
+            //Then
+            Assert.IsNotNull(obj);
+            Assert.IsNotNull(obj.gameObject);
+        }
+    }
 
-		AddTypeToPoolDerivative d2 = ObjectPool.Instance.Acquire<AddTypeToPoolDerivative>();
+    IEnumerator AsyncImmediateInstantiation()
+    {
+        //Given
+        yield return new WaitForEndOfFrame();        
 
-		Assert.AreEqual(ObjectPool.Instance.GetInstanceCountTotal<AddTypeToPoolDerivative>(), 2);
-	}
+        for (int i = 0; i < 20; i++)
+        {
+            AsyncImmediateDerivative obj = ObjectPool.Instance.Acquire<AsyncImmediateDerivative>();
 
-	void PerformanceTest()
-	{
-		ObjectPoolGameObjectTestScript reference = ObjectPool.Instance.Acquire<ObjectPoolGameObjectTestScript>();
+            //Then
+            TestDataClass expected = new TestDataClass(1);
+            Assert.IsTrue(obj.CheckEqual(expected));
+        }
+    }
 
-		Stopwatch without = new Stopwatch();
-		Stopwatch generic = new Stopwatch();
+    IEnumerator AddTypeToPoolTest()
+    {
+        //Given
+        yield return new WaitForEndOfFrame();
 
-		int num = 10000;
+        AddTypeToPoolDerivative d = new AddTypeToPoolDerivative();
+        ObjectPool.Instance.AddTypeToPool(d);
 
-		//Without the use of an object pool
-		without.Start();
-		for (int i = 0; i < num; ++i)
-		{
-			GameObject go = GameObject.Instantiate<GameObject>(reference.gameObject);
+        AddTypeToPoolDerivative d2 = ObjectPool.Instance.Acquire<AddTypeToPoolDerivative>();
 
-			if (i % 2 == 0)
-			{
-				GameObject.Destroy(go);
-			}
-		}
-		without.Stop();
+        //When
+        int count = ObjectPool.Instance.GetInstanceCountTotal<AddTypeToPoolDerivative>();
 
-		//Generic object pool
-		generic.Start();
-		for (int i = 0; i < num; ++i)
-		{
-			ObjectPoolGameObjectTestScript t = ObjectPool.Instance.Acquire<ObjectPoolGameObjectTestScript>();
+        //Then
+        int expected = 2;
+        Assert.AreEqual(count, expected);
+    }
 
-			if (i % 2 == 0)
-			{
-				ObjectPool.Instance.Release<ObjectPoolGameObjectTestScript>(t);
-			}
-		}
-		generic.Stop();
+    void PerformanceTest()
+    {
+        ObjectPoolGameObjectTestScript reference = ObjectPool.Instance.Acquire<ObjectPoolGameObjectTestScript>();
 
-		Debug.Log("Performance, " + num + " entities - Without: " + without.Elapsed);
-		Debug.Log("Performance, " + num + " entities - Generic: " + generic.Elapsed);
-	}
+        Stopwatch without = new Stopwatch();
+        Stopwatch generic = new Stopwatch();
 
-	class TestDataClass
-	{
-		public int myInt;
-		public string myString;
-		public List<string> list = new List<string>();
-		public Vector3 myVector;
+        int num = 10000;
 
-		public TestDataClass() : this(1)
-		{ }
+        //Without the use of an object pool
+        without.Start();
+        for (int i = 0; i < num; ++i)
+        {
+            GameObject go = GameObject.Instantiate<GameObject>(reference.gameObject);
 
-		public TestDataClass(int num)
-		{
-			myInt = num;
-			myString = "Num: " + num.ToString();
-			for (int i = 0; i < myInt; i++)
-			{
-				list.Add(" entry: " + i);
-			}
-			myVector = new Vector3(myInt, -myInt, myInt);
-		}
+            if (i % 2 == 0)
+            {
+                GameObject.Destroy(go);
+            }
+        }
+        without.Stop();
 
-		public bool CheckEqual(TestDataClass data)
-		{
-			bool res = true;
+        //Generic object pool
+        generic.Start();
+        for (int i = 0; i < num; ++i)
+        {
+            ObjectPoolGameObjectTestScript t = ObjectPool.Instance.Acquire<ObjectPoolGameObjectTestScript>();
 
-			if (data.myInt != this.myInt) res = false;
-			if (data.myString != this.myString) res = false;
-			if (data.myVector != this.myVector) res = false;
+            if (i % 2 == 0)
+            {
+                ObjectPool.Instance.Release<ObjectPoolGameObjectTestScript>(t);
+            }
+        }
+        generic.Stop();
 
-			for (int i = 0; i < data.list.Count; i++) if (data.list[i] != this.list[i]) res = false;
+        Debug.Log("Performance, " + num + " entities - Without: " + without.Elapsed);
+        Debug.Log("Performance, " + num + " entities - Generic: " + generic.Elapsed);
+    }
 
-			return res;
-		}
-	}
-	class ThresholdDerivativeDefaultSuccess1 : TestDataClass { }
-	class ThresholdDerivativeDefaultSuccess2 : TestDataClass { }
-	class ThresholdDerivativeIncreased1 : TestDataClass { }
-	class ThresholdDerivativeIncreased2 : TestDataClass { }
-	class ReleaseDerivative1 : TestDataClass { }
-	class ReleaseDerivative2 : TestDataClass { }
-	class AsyncImmediateDerivative : TestDataClass { }
-	class PerformanceDerivative1 : TestDataClass { }
-	class PerformanceDerivative2 : TestDataClass { }
-	class PerformanceDerivative3 : TestDataClass { }
-	class AddTypeToPoolDerivative : TestDataClass { }
+    class TestDataClass
+    {
+        public int myInt;
+        public string myString;
+        public List<string> list = new List<string>();
+        public Vector3 myVector;
+
+        public TestDataClass() : this(1)
+        { }
+
+        public TestDataClass(int num)
+        {
+            myInt = num;
+            myString = "Num: " + num.ToString();
+            for (int i = 0; i < myInt; i++)
+            {
+                list.Add(" entry: " + i);
+            }
+            myVector = new Vector3(myInt, -myInt, myInt);
+        }
+
+        public bool CheckEqual(TestDataClass data)
+        {
+            bool res = true;
+
+            if (data.myInt != this.myInt) res = false;
+            if (data.myString != this.myString) res = false;
+            if (data.myVector != this.myVector) res = false;
+
+            for (int i = 0; i < data.list.Count; i++) if (data.list[i] != this.list[i]) res = false;
+
+            return res;
+        }
+    }
+    class ThresholdDerivativeDefaultSuccess1 : TestDataClass { }
+    class ThresholdDerivativeDefaultSuccess2 : TestDataClass { }
+    class ThresholdDerivativeIncreased1 : TestDataClass { }
+    class ThresholdDerivativeIncreased2 : TestDataClass { }
+    class ReleaseDerivative1 : TestDataClass { }
+    class ReleaseDerivative2 : TestDataClass { }
+    class AsyncImmediateDerivative : TestDataClass { }
+    class PerformanceDerivative1 : TestDataClass { }
+    class PerformanceDerivative2 : TestDataClass { }
+    class PerformanceDerivative3 : TestDataClass { }
+    class AddTypeToPoolDerivative : TestDataClass { }
 }
 
 #endif
